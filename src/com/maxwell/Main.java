838b4c7d8bb6e982e6e87c61fe8b020f5a3a2fb6
Maxwell <maxwell.guru.1@gmail.com>
2021-03-07 14:03:04 -0700

    Added Welcome Text

--- src/com/maxwell/Main.java
+++ src/com/maxwell/Main.java
@@ -3,6 +3,8 @@ package com.maxwell;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("Welcome to the Java Conversion Calculator!");
+        
     }
 }

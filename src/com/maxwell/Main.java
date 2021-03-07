991ba27efc06f14c66478d98134e63cfe0ca9401
Maxwell <maxwell.guru.1@gmail.com>
2021-03-07 14:33:11 -0700

    Added user input for freedom units

--- src/com/maxwell/Main.java
+++ src/com/maxwell/Main.java
@@ -1,11 +1,22 @@
 package com.maxwell;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        //Input declaration
+        Scanner userInput = new Scanner(System.in);
 
+        // Welcome and purpose texts
         System.out.println("Welcome to the Java Conversion Calculator!");
-
         System.out.println("This program converts Freedom units to non-freedom units(feet to meters)");
+
+        //Prompts and input
+        System.out.print("Enter number for feet: ");
+        double feet = userInput.nextDouble();
+
+
+
     }
 }

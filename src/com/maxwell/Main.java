da798404d25be4bbff96242a4d3e49822d9c9bb2
Maxwell <maxwell.guru.1@gmail.com>
2021-03-07 14:05:31 -0700

    Added purpose text

--- src/com/maxwell/Main.java
+++ src/com/maxwell/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Welcome to the Java Conversion Calculator!");
-        
+
+        System.out.println("This program converts Freedom units to non-freedom units(feet to meters)");
     }
 }
